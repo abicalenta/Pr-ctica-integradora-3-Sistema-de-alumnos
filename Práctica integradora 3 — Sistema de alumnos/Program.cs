9687ba20cca4ffca481065c92758d3c1c185dd5b
abigail calenta <abicalenta@gmail.com>
2026-09-14 09:38:08 -0300

    feat(etapa-3): metodos Promedio, EstaAprobado y SubirNota

--- Práctica integradora 3 — Sistema de alumnos/Program.cs
+++ Práctica integradora 3 — Sistema de alumnos/Program.cs
@@ -1,22 +1,25 @@
-﻿public class Alumno
+﻿using System.Xml;
+
+public class Alumno
 {
     public string Nombre { get; set; }
     public int Legajo { get; set; }
     public double Nota1 { get; set; }
     public double Nota2 { get; set; }
+
+    public Alumno(string nombre, int legajo, double Nota1, double Nota2)
+    {
+        Nombre = nombre;
+        Legajo = legajo;
+        Nota1 = Nota1;
+        Nota2 = Nota2;
+    }
 }
 
-Alumno alumno1 = new Alumno();
-alumno1.Nombre = "Ana";
-alumno1.Legajo = 1001;
-alumno1.Nota1 = 8.5;
-alumno1.Nota2 = 9.0;
 
-Alumno alumno2 = new Alumno();
-alumno2.Nombre = "juan";
-alumno2.Legajo = 1002;
-alumno2.Nota1 = 6.0;
-alumno2.Nota2 = 7.0;
+Alumno alumno1 = new Alumno("ana", 1001, 8.5, 9.0);
+Alumno alumno2 = new Alumno("juan", 1002, 6.0, 7.0);
+
 
 Console.WriteLine($"Alumno 1: {alumno1.Nombre} - Legajo: {alumno1.Legajo}");
 Console.WriteLine($"Alumno 2: {alumno1.Nombre} - Legajo: {alumno2.Legajo}");
@@ -26,4 +29,45 @@ alumno1.Nombre = "ana maria";
 Console.WriteLine("\nDespués de modificar alumno1:");
 Console.WriteLine($"Alumno 1: {alumno1.Nombre}");
 Console.WriteLine($"Alumno 2: {alumno2.Nombre}");
+
+public double Promedio()
+{
+    return (Nota1 + Nota2) / 2.0;
+}
+
+public bool EstaAprobado()
+{
+    return Promedio() >= 6.0;
+}
+
+public void SubiNota()
+{
+    if (Nota1 + 1 <= 10)
+    {
+        Nota1 += 1;
+    }
+    else
+    {
+        Nota1 = 10;
+    }
+
+    if (Nota2 + 1 <= 10)
+    {
+        Nota2 += 1;
+    }
+    else
+    {
+        Nota2 = 10;
+    }
+}
+
+
+Alumno alumno1 = new Alumno("Ana Pérez", 1234, 8.0, 6.0);
+Alumno alumno2 = new Alumno("Juan López", 5678, 4.0, 5.0);
+
+Console.WriteLine($"Promedio Ana: {alumno1.Promedio()} - ¿Aprobada?: {alumno1.EstaAprobado()}");
+Console.WriteLine($"Promedio Juan: {alumno2.Promedio()} - ¿Aprobado?: {alumno2.EstaAprobado()}");
+
+alumno2.SubirNota();
+Console.WriteLine($"Juan tras subir nota -> Promedio: {alumno2.Promedio()} - ¿Aprobado?: {alumno2.EstaAprobado()}");
 
