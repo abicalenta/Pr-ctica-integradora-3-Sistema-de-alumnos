94f6b8805ff905271949e8296c5b08f404ecffbd
abigail calenta <abicalenta@gmail.com>
2026-09-20 10:51:41 -0300

    feat(etapa-5): private set y validacion con CargarNotas

--- Práctica integradora 3 — Sistema de alumnos/Program.cs
+++ Práctica integradora 3 — Sistema de alumnos/Program.cs
@@ -3,7 +3,7 @@
 public class Alumno
 {
     public string Nombre { get; set; }
-    public int Legajo { get; set; }
+    public int Legajo { get; private set; }
     public double Nota1 { get; set; }
     public double Nota2 { get; set; }
 
@@ -15,6 +15,16 @@ public class Alumno
         Nota2 = Nota2;
     }
 
+    public bool CargarNotas(double nota1, double nota2)
+    {
+        if (nota1 >= 0 && nota1 <= 10 && nota2 >= 0 && nota2 <= 10)
+        {
+            Nota1 = nota1;
+            Nota2 = nota2;
+            return true;
+        }
+        return false;
+    }
     public double Promedio()
     {
         return (Nota1 + Nota2) / 2.0;
@@ -44,6 +54,11 @@ public class Alumno
             Nota2 = 10;
         }
     }
+
+    public override string ToString()
+    {
+        return $"{Legajo} {Nombre} (promedio: {Promedio()}";
+    }
 }
 
 
@@ -71,3 +86,14 @@ Console.WriteLine($"Promedio Juan: {alumno2.Promedio()} - ¿Aprobado?: {alumno2.
 alumno2.SubirNota();
 Console.WriteLine($"Juan tras subir nota -> Promedio: {alumno2.Promedio()} - ¿Aprobado?: {alumno2.EstaAprobado()}");
 
+Alumno alumno = new Alumno("Ana pérez", 1234);
+
+if (!alumno.CargarNotas(47.0, 8.0))
+{
+    Console.WriteLine("Aviso: Notas invalidas. No se registraron.");
+}
+
+if (alumno.CargarNotas(8.0, 6.0))
+{
+    Console.WriteLine($"Notas cargadas con exito: {alumno}");
+}
